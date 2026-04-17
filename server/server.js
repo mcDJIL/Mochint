c7f3672e7c6e0a987b4b16aa51d6aac7f2730e9e
mcDJIL <mdjauharil29@gmail.com>
2026-04-18 00:54:32 +0700

    fix: correct import statement syntax in server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ const contactRoutes = require('./routes/contactRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
-import { fileURLToPath } from 'url';
 
 app.use(cors({
   origin: '*',
@@ -74,8 +73,6 @@ app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
   next();
 });
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.join(__dirname, '../public_html')));
 
